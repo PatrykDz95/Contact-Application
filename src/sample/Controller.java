9e483f90ee920587c6b89f4ca5a513d9946428ff
Patryk <patrykdziedzioch@gmail.com>
2018-02-05 21:17:07 +0100

    Add some classes and TextField

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,61 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class Controller {
+
+    @FXML
+    TableView<ContactList> table;
+    @FXML
+    private TableColumn firstColumn;
+    @FXML
+    private TableColumn lastNameColumn;
+    @FXML
+    private TableColumn phoneColumn;
+    @FXML
+    private TableColumn notesColumn;
+
+    public void initialize() {
+                 table = new TableView<>();
+                ObservableList<ContactList> data = FXCollections.observableArrayList(
+                new ContactList("Jacob", "Smith", "jacob.smith@example.com", "note"),
+                new ContactList("Isabella", "Johnson", "isabella.johnson@example.com", "note"),
+                new ContactList("Ethan", "Williams", "ethan.williams@example.com", "note"),
+                new ContactList("Emma", "Jones", "emma.jones@example.com", "note"),
+                new ContactList("Michael", "Brown", "michael.brown@example.com", "note")
+        );
+
+
+        firstColumn.setCellValueFactory(
+                new PropertyValueFactory<>("firstName")
+        );
+        lastNameColumn.setCellValueFactory(
+                new PropertyValueFactory<>("lastName")
+        );
+        phoneColumn.setCellValueFactory(
+                new PropertyValueFactory<>("phoneNumber")
+        );
+
+        phoneColumn.setCellValueFactory(
+                new PropertyValueFactory<ContactList, String>("notes")
+        );
+
+        table.setItems(data);
+        table.getColumns().addAll(firstColumn, lastNameColumn, phoneColumn, notesColumn);
+    }
+
+
+//    @FXML
+//    public void showContactsList() {
+//
+//}
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 433.0, 378.0));
+        primaryStage.setScene(new Scene(root, 530.0, 460));
         primaryStage.show();
     }
 
